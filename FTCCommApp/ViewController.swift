9ac3098b877e74654f391a343c9d41fdc8ee3525
Roohit Ravi <rohitravi2001@gmail.com>
2020-07-02 23:43:51 -0700

    Initial Commit

---
+++ FTCCommApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FTCCommApp
+//
+//  Created by Rohit Ravi on 7/2/20.
+//  Copyright © 2020 Rohit Ravi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

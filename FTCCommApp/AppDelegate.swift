dfcfdbbe84a6d868c4be6fdac8ef475f1e5b3fd7
Roohit Ravi <rohitravi2001@gmail.com>
2020-07-03 01:20:33 -0700

    firebasee config add

--- FTCCommApp/AppDelegate.swift
+++ FTCCommApp/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import UserNotifications
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -22,7 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             
         
         }
-        
+        FirebaseApp.configure()
         return true
     }
 
